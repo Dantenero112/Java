1a9087a256758ae15cb1fc68e180d4d45f2523a7
Yahiya <134516066+Dantenero112@users.noreply.github.com>
2026-02-22 22:48:46 +0530

    Add files via upload

---
+++ TestOr.java
@@ -0,0 +1,11 @@
+class TestOR{
+public static void main(String args[])
+{int x=10, y=20, z=30;
+Boolean b=(++x<++y)||(++y<++z);
+System.out.println(b);
+System.out.println(x);
+System.out.println(y);
+System.out.println(z);
+
+}
+}
