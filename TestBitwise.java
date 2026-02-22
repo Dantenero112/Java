483d340339756ca0ec2e78085466013f6a301c44
Yahiya <134516066+Dantenero112@users.noreply.github.com>
2026-02-22 22:59:14 +0530

    Add files via upload

---
+++ TestBitwise.java
@@ -0,0 +1,12 @@
+class TestBitwise{
+public static void main(String args[])
+{byte x=10, y=13;
+ byte a=(byte) (x&y);
+byte b=(byte) (x|y);
+byte c=(byte) (x^y);
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+
+}
+}
---
+++ TestRightShift.java
@@ -0,0 +1,22 @@
+class TestRightShift{
+public static void main(String args[])
+{/*byte x=10;
+byte a=(byte) (x>>1);
+byte b=(byte) (x>>2);
+byte c=(byte) (x>>3);
+System.out.println(x);
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);*/
+byte x=-10;
+byte a=(byte) (x>>1);
+byte b=(byte) (x>>2);
+byte c=(byte) (x>>3);
+byte d=(byte) (x>>4);
+System.out.println(x);
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+System.out.println(d);
+}
+}
