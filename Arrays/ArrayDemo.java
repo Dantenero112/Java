ab0e9816538f16c665cb07935842fcc32aabf242
Yahiya <134516066+Dantenero112@users.noreply.github.com>
2026-05-22 22:48:53 +0530

    Update ArrayDemo.java

--- Arrays/ArrayDemo.java
+++ Arrays/ArrayDemo.java
@@ -1,16 +1,19 @@
-class ArrayDemo{
-	public void printArray(int[] a){
+class ArrayDemo
+{
+	public void printArray(int[] a)
+	{
 	 for(int i=0;i<a.length;i++)
 	 System.out.print(a[i]+" ");
 	 System.out.println();
 	}
 	public static void main(String args[])
-	{int[] x=new int[5];
-	int[] y={2,3,-1,7,2};
-	int[] z=new int[]{2,10,-3,1,20};
-	ArrayDemo ob=new ArrayDemo();
-	ob.printArray(x);
-	ob.printArray(y);
-	ob.printArray(z);
+	{
+		int[] x=new int[5];
+		int[] y={2,3,-1,7,2};
+		int[] z=new int[]{2,10,-3,1,20};
+		ArrayDemo ob=new ArrayDemo();
+		ob.printArray(x);
+		ob.printArray(y);
+		ob.printArray(z);
 	}
 }
