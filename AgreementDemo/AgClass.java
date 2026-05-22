c5ce542a89d925768d6491bcc6780c857787c24a
Yahiya <134516066+Dantenero112@users.noreply.github.com>
2026-05-22 22:47:35 +0530

    Update AgClass.java

--- AgreementDemo/AgClass.java
+++ AgreementDemo/AgClass.java
@@ -1,8 +1,12 @@
-class AgClass implements Agreement{
-	public void sort(int[] a){
+class AgClass implements Agreement
+{
+	public void sort(int[] a)
+	{
 	int temp;
-	for(int i=1;i<a.length;i++){
-	 for(int j=1;j<a.length-i+1;j++){
+	for(int i=1;i<a.length;i++)
+	{
+	 for(int j=1;j<a.length-i+1;j++)
+	 {
 	 	if(a[j-1]>a[j])
 	 	{
 	  	temp=a[j-1];
