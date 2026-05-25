ddd3b7c0cc4c678f32c114bfed208341b140be6c
Yahiya <134516066+Dantenero112@users.noreply.github.com>
2026-05-25 23:59:23 +0530

    ExceptionDemo1.java

--- Exception/ExceptionDemo1.java
+++ Exception/ExceptionDemo1.java
@@ -1,12 +1,12 @@
 import java.util.*;
 class ExceptionDemo1{
-public static void main(String args[]){
-Scanner sc=new Scanner(System.in);
-int[] arr={3,21,34,54,22};
-System.out.print("Enter the number of iterations: ");
-int length=sc.nextInt();
+	public static void main(String args[]){
+	Scanner sc=new Scanner(System.in);
+	int[] arr={3,21,34,54,22};
+	System.out.print("Enter the number of iterations: ");
+	int length=sc.nextInt();
 	for(int i=0;i<length;i++){
-	System.out.print(arr[i]+" ");
+		System.out.print(arr[i]+" ");
+		}	
 	}
-}
 }
