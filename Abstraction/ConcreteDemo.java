832cfefa8ab31cf386ab2e5159f2e23283ab66cf
Yahiya <134516066+Dantenero112@users.noreply.github.com>
2026-05-25 23:57:48 +0530

    ConcreteDemo.java

--- Abstraction/ConcreteDemo.java
+++ Abstraction/ConcreteDemo.java
@@ -1,14 +1,14 @@
 class ConcreteDemo extends AbstractDemo2{
-ConcreteDemo(){
-System.out.println("Constructor of ConcreteDemo class");
-}
-void printer(){
-System.out.println("Printer of Concrete Class");
-}
-void show(){
-System.out.println("Show Method");
-}
-public static AbstractDemo2 getInstance(){
-return new ConcreteDemo();
-}
+  ConcreteDemo(){
+  System.out.println("Constructor of ConcreteDemo class");
+  }
+  void printer(){
+  System.out.println("Printer of Concrete Class");
+  }
+  void show(){
+  System.out.println("Show Method");
+  }
+  public static AbstractDemo2 getInstance(){
+  return new ConcreteDemo();
+  }
 }
