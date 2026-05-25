881d136b6786e40777fcf05ae8a9b497692627b5
Yahiya <134516066+Dantenero112@users.noreply.github.com>
2026-05-25 23:58:31 +0530

    NonAbstractDemo1.java

--- Abstraction/NonAbstractDemo1.java
+++ Abstraction/NonAbstractDemo1.java
@@ -1,5 +1,5 @@
 class NonAbstractDemo1 extends AbstractDemo1{
-NonAbstractDemo1(){
-System.out.println("Constructor of Non Asbtract Class");
-}
+  NonAbstractDemo1(){
+  System.out.println("Constructor of Non Asbtract Class");
+  }
 }
