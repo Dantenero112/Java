fc10b6b0ae6d7d751c9f9e4994091f67f1ae3f07
Yahiya <134516066+Dantenero112@users.noreply.github.com>
2026-05-25 23:56:27 +0530

    AbstractTester.java

--- Abstraction/AbstractTester.java
+++ Abstraction/AbstractTester.java
@@ -1,6 +1,6 @@
 class AbstractTester{
-public static void main(String args[]){
-NonAbstractDemo ob=new NonAbstractDemo();
-ob.printer();
-}
+  public static void main(String args[]){
+  NonAbstractDemo ob=new NonAbstractDemo();
+  ob.printer();
+  }
 }
