fc3ce06cc957845a14c26f06e5b7abb52c3f1464
Yahiya <134516066+Dantenero112@users.noreply.github.com>
2026-06-01 23:54:04 +0530

    Update PolyTest.java

--- Polymorphism/PolyTest.java
+++ Polymorphism/PolyTest.java
@@ -7,23 +7,23 @@ PolyDemo(){}
 PolyDemo(int a){x=a;}
 PolyDemo(int a, int b){x=a;y=b;}
 void display(){System.out.println(x+" "+y);}*/
-
 PolyDemo(int a,int b){x=a;y=b;}
 int add(int a,int b){return a+b;}
 int add(int a,int b, int c){ return a+b+c;}
 }
+
 class PolyTest{
-public static void main(String args[]){
-/*PolyDemo pd=new PolyDemo();
-PolyDemo pd2=new PolyDemo(10);
-PolyDemo pd3=new PolyDemo(15,30);
-pd.display();
-pd2.display();
-pd3.display();*/
-PolyDemo pd=new PolyDemo(10,20);
-int a=pd.add(pd.x,pd.y);
-int b=pd.add(pd.x,pd.y,30);
-System.out.println(a);
-System.out.println(b);
-}
+  public static void main(String args[]){
+  /*PolyDemo pd=new PolyDemo();
+  PolyDemo pd2=new PolyDemo(10);
+  PolyDemo pd3=new PolyDemo(15,30);
+  pd.display();
+  pd2.display();
+  pd3.display();*/
+  PolyDemo pd=new PolyDemo(10,20);
+  int a=pd.add(pd.x,pd.y);
+  int b=pd.add(pd.x,pd.y,30);
+  System.out.println(a);
+  System.out.println(b);
+  }
 }
