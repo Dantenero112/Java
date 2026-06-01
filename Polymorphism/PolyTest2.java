0d232c451e7b9ecfaf123f31d90f617ed561cc49
Yahiya <134516066+Dantenero112@users.noreply.github.com>
2026-06-01 23:54:29 +0530

    Update PolyTest2.java

--- Polymorphism/PolyTest2.java
+++ Polymorphism/PolyTest2.java
@@ -8,9 +8,10 @@ PolyDemo2(int a, int b){this(a);y=b;}
 void display(){System.out.println(x+" "+y);}
 
 }
+
 class PolyTest2{
 public static void main(String args[]){
-PolyDemo2 pd=new PolyDemo2(5,7);
-pd.display();
-}
+  PolyDemo2 pd=new PolyDemo2(5,7);
+  pd.display();
+  }
 }
