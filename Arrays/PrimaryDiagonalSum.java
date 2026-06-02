eb0429628b2c5f9ad5a09bfeaf122018b29159c6
Yahiya <134516066+Dantenero112@users.noreply.github.com>
2026-06-02 23:33:12 +0530

    Update PrimaryDiagonalSum.java

--- Arrays/PrimaryDiagonalSum.java
+++ Arrays/PrimaryDiagonalSum.java
@@ -2,7 +2,8 @@
 class PrimaryDiagonalSum
 {
    public static void main(String args[])
-   { int sum=0;
+   { 
+	int sum=0;
 	int[][] td={{1,2,3,4},{2,4,6,8},{10,20,30,40},{11,22,33,44}};
 	for(int i=0;i<td.length;i++)
 	{
